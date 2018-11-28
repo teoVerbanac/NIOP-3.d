473f65b800c2c6ac22e1ddf07002a3e9f724ff2a
teoVerbanac <44996650+teoVerbanac@users.noreply.github.com>
2018-11-28 11:34:03 +0100

    Add files via upload

---
+++ VerbanacTeoVrijeme/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerbanacTeoVrijeme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Nova = new DateTime(DateTime.Now.Year +1,1,1);
+          TimeSpan preostalo;
+            preostalo = Nova - DateTime.Now;
+            Console.WriteLine("Do Nove godine preostalo je:");
+            Console.WriteLine("{0}dana, {1}sati, {2}minuta , {3} sekundi", preostalo.Days, preostalo.Hours, preostalo.Minutes, preostalo.Seconds);
+            Console.WriteLine("U danima: {0}", preostalo.TotalDays);
+            Console.WriteLine("U satima: {0}", preostalo.TotalHours);
+            Console.WriteLine("U minutama: {0}", preostalo.TotalMinutes);
+            Console.WriteLine("U sekundama: {0}", preostalo.TotalSeconds);
+            Console.ReadKey();
+
+        }
+    }
+}
